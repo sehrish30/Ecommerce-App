79486767782c488cfd929ac156de1580b8f075ea
sehrish30 <sehrishwaheed98@gmail.com>
2021-08-23 23:56:13 +0300

    Google login

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { auth } from "../../firebase";
+import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
 
-import { MailOutlined } from "@ant-design/icons";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 
 import { useDispatch } from "react-redux";
 
@@ -36,6 +36,27 @@ const Login = ({ history }) => {
       toast.error(err.message);
       setLoading(false);
     }
+  };
+
+  const googleLogin = async () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
+      });
   };
 
   const loginForm = () => (
@@ -80,8 +101,23 @@ const Login = ({ history }) => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>Login</h4>
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Login</h4>
+          )}
           {loginForm()}
+          <Button
+            block
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+            onClick={googleLogin}
+            type="danger"
+            className="mb-3"
+          >
+            Login with Google
+          </Button>
         </div>
         <div className="col-md-6 offset-md-3"></div>
       </div>
